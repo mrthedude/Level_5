bb156a693340409754f3b6c8296f67f2f288dce2
mrthedude <mrthedude429@gmail.com>
2024-03-19 13:16:53 -0400

    added the ERC20.sol file for an initial collateral to lend against in Lending.sol

---
+++ src/ERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract token is ERC20, Ownable {
+    constructor(address owner) ERC20("Level5", "_LVL_5") Ownable(owner) {
+        _mint(owner, 100000 * 10 ** decimals());
+    }
+}
