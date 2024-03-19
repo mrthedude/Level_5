3055be3758bd579ea74e857d0aff41fe34fa9d0c
mrthedude <mrthedude429@gmail.com>
2024-03-19 13:24:35 -0400

    added priceConverter import

--- src/Lending.sol
+++ src/Lending.sol
@@ -29,6 +29,7 @@ pragma solidity ^0.8.19;
 /////////////////
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {priceConverter} from "./priceConverter.sol";
 
 /////////////////
 //// Errors ////
