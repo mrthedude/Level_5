836bf26baaf99b33f69cc5034ff7642a0cf6a359
mrthedude <mrthedude429@gmail.com>
2024-04-12 19:50:02 -0400

    added Deploy_Lending.s.sol

--- script/Deploy_Lending.s.sol
+++ script/Deploy_Lending.s.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {token} from "../src/ERC20_token.sol";
+import {lending} from "../src/Lending.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract lendingDeployer is Script, HelperConfig {
+    function run() public returns (lending, token) {
+        HelperConfig helperConfig = new HelperConfig();
+        address owner = helperConfig.getOwnerAddress();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        vm.startBroadcast();
+        token myToken = new token(owner);
+        lending LendingContract = new lending(owner, ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return (LendingContract, myToken);
+    }
+}
