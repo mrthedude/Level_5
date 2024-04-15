1716e29388116e13a98d25d3546673c4b826b609
mrthedude <mrthedude429@gmail.com>
2024-04-15 14:16:21 -0400

    edited import paths

--- src/ERC20_token.sol
+++ src/ERC20_token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract token is ERC20, Ownable {
     constructor(address owner) ERC20("Level5", "_LVL_5") Ownable(owner) {
