2d19ad49ef96c7a45995b31a61e941d0f05e702c
mrthedude <mrthedude429@gmail.com>
2024-04-15 10:38:41 -0400

    added comments

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -45,12 +45,12 @@ contract HelperConfig is Script {
 
     function getOwnerAddress() public view returns (address _owner) {
         if (block.chainid == 534351) {
-            // Scroll Sepolia
+            // Scroll Sepolia -> burner_addy
             return 0x6a571992ECaaDe9df63334BACEdD46C7C78e3Ef9;
         }
 
         if (block.chainid == 31337) {
-            // Anvil
+            // Anvil -> default foundry addy
             return 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
         }
     }
