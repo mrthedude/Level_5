e3591de548b181bcc78126ec3b4b6aba0c70d415
mrthedude <mrthedude429@gmail.com>
2024-04-17 10:53:06 -0400

    added the testFuzz_logicIsCorrectWhenRepayAmountIsLessThanBorrowingFees() test

--- test/unit/Lending_Test.t.sol
+++ test/unit/Lending_Test.t.sol
@@ -266,4 +266,18 @@ contract Lending_Test is Test, lendingDeployer {
         lendingContract.repay{value: 0.02626 ether}(myToken);
         vm.stopPrank();
     }
+
+    function testFuzz_logicIsCorrectWhenRepayAmountIsLessThanBorrowingFees(uint256 repayAmount) public {
+        vm.assume(repayAmount > 0 && repayAmount < 0.00125 ether);
+        vm.startPrank(contractOwner);
+        lendingContract.allowTokenAsCollateral(myToken, 200e18);
+        (bool success,) = address(lendingContract).call{value: 0.5 ether}("");
+        require(success, "transfer failed");
+        myToken.approve(address(lendingContract), 105e18);
+        lendingContract.deposit(myToken, 105e18);
+        lendingContract.borrow(myToken, 0.025 ether);
+        lendingContract.repay{value: repayAmount}(myToken);
+        vm.stopPrank();
+        vm.assertEq(contractOwner.balance, STARTING_USER_BALANCE - 0.5 ether + 0.025 ether - repayAmount);
+    }
 }
